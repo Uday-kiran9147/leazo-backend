3bae43f995209b030635a3c0d81b4610e7377608
Uday-kiran9147 <udaykiran9147@gmail.com>
2026-08-08 20:39:52 +0530

    fix: set NODE_ENV to production in PM2 commands and update start:prod script

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,6 +5,10 @@ module.exports = {
       script: './dist/src/main.js',
       instances: 1,
       exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      },
       env_production: {
         NODE_ENV: 'production',
         PORT: 3000
